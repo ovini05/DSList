446e55e363fcd0833c375c9a4efe6c1145654d19
Vinicius Fernandes <viniviniciusfernandes15@gmail.com>
2025-01-20 20:27:45 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,26 +18,32 @@ public class Game {
 	private Long id;
 	private String title;
 
-	@Column(name = "geme_year")
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
-	private String imlUrl;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, int year, String genre, String platform, String imlUrl, String shortDescription,
-			String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
-		this.imlUrl = imlUrl;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -74,20 +80,28 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImlUrl() {
-		return imlUrl;
+		return imgUrl;
 	}
 
-	public void setImlUrl(String imlUrl) {
-		this.imlUrl = imlUrl;
+	public void setImlUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
